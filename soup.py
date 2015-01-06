254e2ad517509f4d1fa7bd9fccfb5f83dcfa23cd
Jason Ervin <jjervin@gmail.com>
2015-01-06 00:55:56 -0600

    Text scraper scrollbar fixed
    Text scraper vertical scrollbar has been moved to the right side

--- soup.py
+++ soup.py
@@ -12,9 +12,10 @@ bod.pack()
 screen = Frame(root, bg='white')
 screen.pack()
 
+
 #*** Guts ***
 def Textscrape():
-    """Scrapes destination provided by user and returns text in new text window"""
+    """Scrapes destination provided by user and returns text in window"""
 
     url = UrlEntry.get()
     r = requests.get(url)
@@ -22,16 +23,16 @@ def Textscrape():
     soup = BeautifulSoup(data)
     scrape = soup.text
     outputtext = Text(screen)
-    outputtext.pack(side=RIGHT)
+    outputtext.pack(side=LEFT)
     outputtext.insert(END, scrape)
-    sb = Scrollbar(screen)
-    sb.pack(side=RIGHT, fill=Y)
-    sb.config(command=outputtext.yview)
-    outputtext.config(yscrollcommand=sb.set)
+    sb1 = Scrollbar(screen)
+    sb1.pack(side=RIGHT, fill=Y)
+    sb1.config(command=outputtext.yview)
+    outputtext.config(yscrollcommand=sb1.set)
 
 
 def linkscrape():
-    """Scrapes destination provided by user and returns links in new text window"""
+    """Scrapes destination provided by user and returns links in window"""
 
     url = UrlEntry.get()
     r = requests.get(url)
@@ -40,13 +41,18 @@ def linkscrape():
     for link in soup.find_all('a'):
         links = (link.get('href'))
         print links
-        output_text2 = Text(screen)
-        output_text2.pack(side=BOTTOM)
-        output_text2.insert(10.10, links)
-    sb = Scrollbar(screen)
-    sb.pack(side=RIGHT, fill=Y)
-    sb.config(command=output_text2.yview)
-    output_text2.config(yscrollcommand=sb.set)
+
+
+def printscrape():
+    links2 = linkscrape()
+    print links2
+    sb2 = Scrollbar(screen)
+    sb2.pack(side=RIGHT, fill=Y)
+    output_text2 = Text(screen, yscrollcommand=sb2.set)
+    output_text2.pack(side=TOP)
+    output_text2.insert(10.10, links2)
+    sb2.config(command=output_text2.yview)
+
 
 #*** Label ***
 UrlLabel = Label(bod, text='Please enter URL here:', bg='grey', fg='black')
@@ -58,15 +64,11 @@ UrlEntry.grid(row=1, column=1)
 
 #*** Buttons ***
 UrlSubmit = ttk.Button(bod, text='Text', command=Textscrape)
-UrlSubmit2 = ttk.Button(bod, text='Links', command=linkscrape)
+UrlSubmit2 = ttk.Button(bod, text='Links', command=printscrape)
 UrlCancel = ttk.Button(bod, text='Cancel', command=root.quit)
 UrlSubmit.grid(row=1, column=3)
 UrlSubmit2.grid(row=1, column=4)
 UrlCancel.grid(row=1, column=5)
-
-
-
-
 
 
 
